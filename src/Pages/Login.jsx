6f16e00b41908ebfcee6238489ea9ac4d18160e8
jobayer561 <jobayer3085@gmail.com>
2025-11-12 11:34:07 +0600

    login page created

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Login = () => {
+  return (
+    <div
+      className="min-h-screen px-4 flex justify-center items-center   bg-linear-to-r from-[#ff512f]/90 via-[#ff2a68] to-[#dd2476]/90
+ animate-gradient"
+    >
+      <div className="bg-white/90 w-[450px] p-6 rounded-2xl shadow-2xl  border border-white/20">
+        <div className="text-center mb-3">
+          <h1 className="text-2xl font-bold mt-2 text-gray-800">Login</h1>
+        </div>
+
+        <form>
+          <div className="mb-3 ">
+            <label className=" text-gray-500 text-sm  ">Email</label>
+            <input
+              type="text"
+              placeholder="Enter Your email"
+              required
+              className="w-full px-5 py-3 rounded-full bg-white shadow-md focus:outline-none "
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className=" text-gray-500 text-sm ">Password</label>
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              required
+              className="w-full px-5 py-3 rounded-full bg-white shadow-md focus:outline-none"
+            />
+          </div>
+
+          <div className="flex justify-between items-center text-sm mb-6">
+            <a href="#" className="text-gray-600 hover:text-red-500">
+              Forgot password?
+            </a>
+          </div>
+
+          <button className="w-full py-3 rounded-full bg-linear-to-r from-red-500 to-orange-500 text-white font-semibold   shadow-lg hover:shadow-xl hover:scale-105 ">
+            Login
+          </button>
+
+          <div className="flex items-center my-5 text-gray-400 text-sm">
+            <div className="flex-1 h-px bg-gray-300"></div>
+            <span className="mx-3">or continue with</span>
+            <div className="flex-1 h-px bg-gray-300"></div>
+          </div>
+
+          <div className="flex justify-center gap-4">
+            <button className="btn w-full  rounded-full hover:scale-105 transition-transform bg-white text-black border-[#e5e5e5]">
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Login with Google
+            </button>{" "}
+          </div>
+
+          <p className="text-center text-gray-500 text-sm mt-6">
+            Don't have an account?{" "}
+            <Link
+              href="#"
+              className="text-red-500 font-semibold hover:text-orange-500"
+            >
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
