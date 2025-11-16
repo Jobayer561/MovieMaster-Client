42f9ee209879b597ef9b6a41d331411dc4b48cb4
jobayer561 <jobayer3085@gmail.com>
2025-11-16 19:31:43 +0600

    all movies size update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,38 +27,48 @@ const Home = () => {
       <Carousel />
 
       <motion.div
-        className="text-white p-6 rounded-lg flex flex-col sm:flex-row gap-6 justify-center items-center mt-4"
+        className="text-white p-6 rounded-lg flex flex-col md:flex-row gap-6 justify-center items-center mt-4 max-w-7xl mx-auto min-h-[250px]"
         initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
         viewport={{ once: true }}
       >
         <motion.div
-          className="bg-[#170F29] flex items-center gap-4 p-6 rounded-md w-64 shadow-md border border-violet-900/80 hover:scale-105 transition-transform"
+          className="
+      bg-gray-600 flex items-center justify-center gap-4 p-6 rounded-md 
+      w-full md:w-1/2 h-[180px]
+      shadow-md border border-violet-900/80 
+      hover:scale-105 transition-transform
+    "
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
           <div className="bg-[#2c2c2c] p-3 rounded-md">
-            <FaFilm className="text-yellow-400 text-2xl" />
+            <FaFilm className="text-yellow-400 text-4xl" />
           </div>
-          <div>
-            <p className="text-sm text-gray-400">Total Movies</p>
+          <div className="text-center">
+            <p className="text-xl font-semibold">Total Movies</p>
             <h2 className="text-3xl font-bold">{stats.totalMovies}</h2>
           </div>
         </motion.div>
 
         <motion.div
-          className="bg-[#170F29] flex items-center gap-4 p-6 rounded-md w-64 border border-violet-900/80 shadow-md hover:scale-105 transition-transform"
+          className="
+      bg-gray-600 flex items-center justify-center gap-4 p-6 rounded-md 
+      w-full md:w-1/2 h-[180px]
+      border border-violet-900/80 shadow-md 
+      hover:scale-105 transition-transform
+    "
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
           <div className="bg-[#2c2c2c] p-3 rounded-md">
-            <FaUsers className="text-blue-400 text-2xl" />
+            <FaUsers className="text-blue-400 text-4xl" />
           </div>
-          <div>
-            <p className="text-sm text-gray-400">Total Users</p>
+          <div className="text-center">
+            <p className="text-xl font-semibold">Total Users</p>
             <h2 className="text-3xl font-bold">{stats.totalUsers}</h2>
           </div>
         </motion.div>
